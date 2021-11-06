7e3242b557a3d77f51fd644153267112b62255c9
David Kajpust <kajpustd@gmail.com>
2021-11-05 20:36:45 -0400

    Setup hardhat. Deploy contract to rinkeby

--- contracts/contracts/PersonalSubgraphAnchor.sol
+++ contracts/contracts/PersonalSubgraphAnchor.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.9;
 
 contract PersonalSubgraphAnchor {
     event AddERC20(address user, address token);
-    event AddERC20Sender(address _governance);
-    event AddERC721(address _sushi);
-    event AddERC721Sender(uint256 _fee);
+    event AddERC20Sender(address user, address token);
+    event AddERC721(address user, address token, uint256 tokenID);
+    event AddERC721Sender(address user, address token, uint256 tokenID);
 
     function addERC20Sender(address _token) external {
         _addERC20(msg.sender, _token);
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,19 +1,35 @@
-// SPDX-License-Identifier: MIT
-
 import * as dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
 
 import "hardhat/types/runtime";
-import { HardhatUserConfig } from "hardhat/types";
-import "@nomiclabs/hardhat-etherscan";
-
-// Plugins
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 
+import { HardhatUserConfig } from "hardhat/types";
+import { providers, Signer } from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { task } from "hardhat/config";
+import { PersonalSubgraphAnchor__factory } from "../contracts/build/types/factories/PersonalSubgraphAnchor__factory";
+
 dotenv.config();
+
+export interface Account {
+  readonly signer: Signer;
+  readonly address: string;
+}
+
+export const getAccounts = async (
+  hre: HardhatRuntimeEnvironment
+): Promise<Account[]> => {
+  const accounts = [];
+  const signers: Signer[] = await hre.ethers.getSigners();
+  for (const signer of signers) {
+    accounts.push({ signer, address: await signer.getAddress() });
+  }
+  return accounts;
+};
 
 const DEFAULT_TEST_MNEMONIC =
   "myth like bonus scare over problem client lizard pioneer submit female collect";
@@ -25,6 +41,25 @@ function getAccountMnemonic() {
 function getDefaultProviderURL(network: string) {
   return `https://${network}.infura.io/v3/${process.env.INFURA_KEY}`;
 }
+
+export const deployContracts = async (
+  hre: HardhatRuntimeEnvironment,
+  signer?: Signer
+): Promise<void> => {
+  const PSANCHOR = (await hre.ethers.getContractFactory(
+    "PersonalSubgraphAnchor",
+    signer
+  )) as PersonalSubgraphAnchor__factory;
+  const psAnchor = await PSANCHOR.deploy();
+  console.log("info", `PersonalSubgraphAnchor deployed: ${psAnchor.address}`);
+};
+
+task("deploy", "Deploy the contract").setAction(
+  async ({}, hre: HardhatRuntimeEnvironment) => {
+    const accounts = await getAccounts(hre);
+    await deployContracts(hre, accounts[0].signer);
+  }
+);
 
 const config: HardhatUserConfig = {
   paths: {
@@ -68,6 +103,15 @@ const config: HardhatUserConfig = {
       gasPrice: "auto",
       blockGasLimit: 30000000,
       url: getDefaultProviderURL("mainnet"),
+      accounts: {
+        mnemonic: getAccountMnemonic(),
+      },
+    },
+    rinkeby: {
+      chainId: 4,
+      gas: 15000000,
+      gasPrice: "auto",
+      url: getDefaultProviderURL("rinkeby"),
       accounts: {
         mnemonic: getAccountMnemonic(),
       },
